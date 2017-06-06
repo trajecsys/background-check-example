45a3a20e2d4dd83e844fdcea2a48d08a714c8a71
C.Jay Martin <cjay.martin@gmail.com>
2017-06-06 12:35:07 -0400

    Completed First Release

--- BackgroundCheckExample/Form1.Designer.cs
+++ BackgroundCheckExample/Form1.Designer.cs
@@ -28,12 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.txtApiKey = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtProgramCode = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtProvider = new System.Windows.Forms.TextBox();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.btnSendUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(86, 6);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(408, 20);
+            this.txtURL.TabIndex = 1;
+            this.txtURL.Text = "https://www.trajecsys.com/programs/api/verification/ProvideData.ashx";
+            // 
+            // txtApiKey
+            // 
+            this.txtApiKey.Location = new System.Drawing.Point(86, 32);
+            this.txtApiKey.Name = "txtApiKey";
+            this.txtApiKey.Size = new System.Drawing.Size(408, 20);
+            this.txtApiKey.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "API Key:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Provider:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(86, 84);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(408, 20);
+            this.txtEmail.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(0, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Student Email:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(0, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Status:";
+            // 
+            // txtProgramCode
+            // 
+            this.txtProgramCode.Location = new System.Drawing.Point(86, 136);
+            this.txtProgramCode.Name = "txtProgramCode";
+            this.txtProgramCode.Size = new System.Drawing.Size(408, 20);
+            this.txtProgramCode.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(0, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Program Code:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(0, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 12;
+            // 
+            // txtProvider
+            // 
+            this.txtProvider.Location = new System.Drawing.Point(86, 58);
+            this.txtProvider.Name = "txtProvider";
+            this.txtProvider.Size = new System.Drawing.Size(408, 20);
+            this.txtProvider.TabIndex = 4;
+            this.txtProvider.Text = "background";
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.AutoCompleteCustomSource.AddRange(new string[] {
+            "InProgress",
+            "Complete",
+            "Alert",
+            "Cancelled"});
+            this.cmbStatus.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cmbStatus.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Items.AddRange(new object[] {
+            "InProgress",
+            "Complete",
+            "Alert",
+            "Cancelled"});
+            this.cmbStatus.Location = new System.Drawing.Point(86, 110);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(408, 21);
+            this.cmbStatus.TabIndex = 8;
+            // 
+            // btnSendUpdate
+            // 
+            this.btnSendUpdate.Location = new System.Drawing.Point(12, 162);
+            this.btnSendUpdate.Name = "btnSendUpdate";
+            this.btnSendUpdate.Size = new System.Drawing.Size(115, 23);
+            this.btnSendUpdate.TabIndex = 15;
+            this.btnSendUpdate.Text = "POST Update";
+            this.btnSendUpdate.UseVisualStyleBackColor = true;
+            this.btnSendUpdate.Click += new System.EventHandler(this.btnSendUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(506, 190);
+            this.Controls.Add(this.btnSendUpdate);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.txtProvider);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtProgramCode);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtApiKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Background Check Example";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.TextBox txtApiKey;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtProgramCode;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtProvider;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.Button btnSendUpdate;
     }
 }
 
--- BackgroundCheckExample/Form1.cs
+++ BackgroundCheckExample/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,58 @@ namespace BackgroundCheckExample
 {
     public partial class Form1 : Form
     {
+        Dictionary<string, string> statusDataSource = new Dictionary<string, string>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSendUpdate_Click(object sender, EventArgs e)
+        {
+            var btnText = btnSendUpdate.Text;
+            btnSendUpdate.Text = "Please Wait...";
+            btnSendUpdate.Enabled = false;
+
+
+            var client = new WebClient();
+
+            client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+            var url = txtURL.Text;
+
+            var reqparm = new System.Collections.Specialized.NameValueCollection();
+            reqparm.Add("apikey", txtApiKey.Text);
+            reqparm.Add("status", cmbStatus.Text);
+
+            var td = DateTime.Now.ToLongDateString();
+            var tdp = DateTime.Parse(td);
+
+            reqparm.Add("status_date", DateTime.Now.ToLongDateString());
+            reqparm.Add("provider", txtProvider.Text);
+            reqparm.Add("student_email", txtEmail.Text);
+
+            // This version not yet implemented
+            // reqparm.Add("program_code", txtProgramCode.Text);
+
+            reqparm.Add("promo_code", txtProgramCode.Text);
+
+            var result = "";
+            try { 
+                var rawResult = client.UploadValues(url, "POST", reqparm);
+                result = Encoding.UTF8.GetString(rawResult);
+            } catch(WebException ex) {
+                result = ex.ToString();
+            };
+
+            MessageBox.Show(result);
+
+            btnSendUpdate.Text = btnText;
+            btnSendUpdate.Enabled = true;
         }
     }
 }
